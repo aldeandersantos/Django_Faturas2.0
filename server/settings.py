8bf4c1d8cafcff8fc1188e19c78851c900361339
Aldeander <aldeander-santos@hotmail.com>
2024-11-13 17:08:35 -0300

    refact/ajustando comentário

--- server/settings.py
+++ server/settings.py
@@ -49,7 +49,7 @@ TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
         'DIRS': [
-            BASE_DIR / 'templates',  # Caminho para a pasta de templates globais
+            BASE_DIR / 'templates',
         ],
         'APP_DIRS': True,
         'OPTIONS': {
