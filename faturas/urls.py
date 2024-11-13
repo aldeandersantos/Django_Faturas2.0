722a26b4dfbdfe477d8900f63229b79dfaab32a9
Aldeander <aldeander-santos@hotmail.com>
2024-11-13 13:24:35 -0300

    feat/templates em home e login

--- faturas/urls.py
+++ faturas/urls.py
@@ -4,7 +4,7 @@ from faturas.views import *
 app_name = 'faturas'
 
 urlpatterns = [
-    path('', home, name='index'),
+    path('', home, name='home'),
     path('cadastrar/', cadastrar_compra, name='cadastrar_compra'),
     path('lista/', lista_compras, name='lista_compras'),
     path('fatura/<int:mes>/<int:ano>/', ver_fatura, name='ver_fatura'),
--- server/urls.py
+++ server/urls.py
@@ -1,13 +1,8 @@
 from django.contrib import admin
 from django.urls import path, include
-from django.views.generic.base import RedirectView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('faturas.urls')),
     path('accounts/', include('django.contrib.auth.urls')),
 ]
-urlpatterns += [
-    path('accounts/login/index.html', RedirectView.as_view(url='/')),
-    path('login.html', RedirectView.as_view(url='/accounts/login/')),
-    ]
