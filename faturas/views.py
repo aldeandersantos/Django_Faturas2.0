37d94d96d64fb4622fd67548875551622b44df2f
Aldeander <aldeander-santos@hotmail.com>
2024-11-17 16:18:38 -0300

    build/add pandas

--- faturas/views.py
+++ faturas/views.py
@@ -1,6 +1,10 @@
+import pandas as pd
+
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
+from django.contrib.admin.views.decorators import staff_member_required
 from .models import Compra
+from django.http import HttpResponse
 from faturas.forms import CompraForm
 from faturas.service import *
 from faturas.utils import mes_ano_atual
@@ -61,4 +65,30 @@ def ver_compras(request):
         if not compra.compra_parcelada:
             compra.parcelas = ''
     return render(request, 'faturas/tables.html', {'compras': compras})
+
+@staff_member_required
+def exportar_fatura_excel(request):
+    mes = request.GET.get('mes', None)
+    ano = request.GET.get('ano', None)
+    if mes is None or ano is None:
+        mes, ano = mes_ano_atual()
+    faturas, total = gera_fatura(request, mes, ano)
+    
+    data = []
+    for fatura in faturas:
+        data.append({
+            'ID': fatura.id,
+            'Descrição': fatura.descricao,
+            'Valor': fatura.valor,
+            'Data': fatura.data,
+            'Usuário': fatura.usuario.username,
+        })
+    df = pd.DataFrame(data)
+    
+    response = HttpResponse(content_type='application/vnd.openxmlformats-officedocument.spreadsheetml.sheet')
+    response['Content-Disposition'] = f'attachment; filename=fatura_{mes}_{ano}.xlsx'
+    with pd.ExcelWriter(response, engine='xlsxwriter') as writer:
+        df.to_excel(writer, index=False, sheet_name='Faturas')
+    
+    return response
 
